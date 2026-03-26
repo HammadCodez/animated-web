c6c63db34ce85df2252e0881c236708392416781
hammadkodez <hammadkodez@gmail.com>
2026-03-26 19:14:54 +0500

    continue the game

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="min-h-screen w-screen bg-amber-400">
+        <video src="./hero-video"></video>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
